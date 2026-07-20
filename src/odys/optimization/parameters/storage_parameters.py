62cf9981750514cbbc10402c616bd77be6a57448
Uday Taneja <udaytanejajee@gmail.com>
2026-07-20 18:45:55 +0530

    Add self-discharge rate to storage SOC dynamics

--- src/odys/optimization/parameters/storage_parameters.py
+++ src/odys/optimization/parameters/storage_parameters.py
@@ -38,6 +38,10 @@ class StorageParameters:
             "max_power": [storage.max_power for storage in self._storages],
             "efficiency_charging": [storage.efficiency_charging for storage in self._storages],
             "efficiency_discharging": [storage.efficiency_discharging for storage in self._storages],
+            "self_discharge_rate": [
+            storage.self_discharge_rate if storage.self_discharge_rate is not None else 0.0
+            for storage in self._storages
+            ],
             "soc_start": [storage.soc_start for storage in self._storages],
             "soc_end": [storage.soc_end for storage in self._storages],
             "soc_min": [storage.soc_min for storage in self._storages],
@@ -78,6 +82,11 @@ class StorageParameters:
     def efficiency_discharging(self) -> xr.DataArray:
         """Return storage discharging efficiency data."""
         return self._dataset["efficiency_discharging"]
+    
+    @property
+    def self_discharge_rate(self) -> xr.DataArray:
+        """Return storage self discharge rate data."""
+        return self._dataset["self_discharge_rate"]
 
     @property
     def soc_start(self) -> xr.DataArray:
